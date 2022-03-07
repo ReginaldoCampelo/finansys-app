a107a67994f450115b4b356fc991663ac7effb27
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-07 14:29:50 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb(){
+    const categories = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de Saúde, plano dentário e remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, shopping e etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de salário'},
+      { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'},
+      { id: 6, name: 'Estudos', description: 'Faculdade, cursos e livros'},
+      { id: 7, name: 'Alimentação', description: 'Mercantil e lanches'}
+    ];
+
+    return { categories }
+  }
+}
