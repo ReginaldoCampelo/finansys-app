4810aee907291f02faa0dae2907dcc3a0cebc31d
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-08 18:13:08 -0300

    adding bread-crumb component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 
 @NgModule({
  
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
+
+  declarations: [
+    BreadCrumbComponent
+  ],
+
   exports: [
     // shared modules
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule,
+
+    // shared components
+    BreadCrumbComponent
   ],
-  declarations: [],
+
 })
 export class SharedModule { }
