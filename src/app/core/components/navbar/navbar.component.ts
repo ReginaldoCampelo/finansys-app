3af3474f15a7f9f335f1e481c266d0e65ab2dd51
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-08 19:50:34 -0300

    extracting NavBarComponent && adding FooterComponent

---
+++ src/app/core/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fns-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule } from "@angular/router";
 
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDatabase } from "../in-memory-database";
 import { FontAewsomeComponent } from './components/font-aewsome/font-aewsome.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
 
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     CommonModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
@@ -21,9 +24,14 @@ import { FontAewsomeComponent } from './components/font-aewsome/font-aewsome.com
     // shared modules
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     HttpClientModule,
-    FontAewsomeComponent
+    FontAewsomeComponent,
+
+    // shared components
+    NavbarComponent,
+    FooterComponent
   ],
-  declarations: [FontAewsomeComponent]
+  declarations: [FontAewsomeComponent, NavbarComponent, FooterComponent]
 })
 export class CoreModule { }
